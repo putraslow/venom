e94add55e2641f59c3ba9701aba5f84979719202
putraslow <67357755+putraslow@users.noreply.github.com>
2020-07-02 12:30:10 +0700

    Add files via upload

---
+++ venom.sh
@@ -0,0 +1,42 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Xilentz
+echo $green"Selamat Datang"
+echo
+sleep 2
+echo $yellow"Silahkan pilih tools nya :"
+echo
+echo $cyan"1.) Stabilkan Jaringan"
+echo
+echo $blue"0.) Keluar aja !!!"
+echo
+echo $white
+read -p "PilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Xilentz
+echo $green"Stabilkan Jaringan by Xilentz"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yeloow
+figlet "Bye"
+echo $white"To Be Continued"
+sleep 3
+exit
+fi
+
